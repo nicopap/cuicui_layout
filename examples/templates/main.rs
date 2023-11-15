eacc2a34f5a4ddfcb6d104ce1d2436374bbc18a7
Nicola Papale <nico@nicopap.ch>
2023-11-15 11:59:08 +0100

    Use cuicui_examples for templates logging

--- examples/templates/main.rs
+++ examples/templates/main.rs
@@ -4,24 +4,16 @@ use cuicui_chirp::ChirpBundle;
 use cuicui_layout::LayoutRootCamera;
 use cuicui_layout_bevy_ui::UiDsl;
 
+fn bevy_log_plugin() -> bevy::log::LogPlugin {
+    cuicui_examples::log_plugin(cfg!(feature = "advanced_logging"))
+}
+
 fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins
                 .set(AssetPlugin { file_path: "../../assets".to_owned(), ..default() })
-                .set(bevy::log::LogPlugin {
-                    level: bevy::log::Level::INFO,
-                    filter: "\
-                    cuicui_chirp=trace,\
-                    cuicui_layout_bevy_ui=info,\
-                    gilrs_core=info,\
-                    gilrs=info,\
-                    naga=info,\
-                    wgpu=error,\
-                    wgpu_hal=error\
-                    "
-                    .to_string(),
-                }),
+                .set(bevy_log_plugin()),
             cuicui_chirp::loader::Plugin::new::<UiDsl>(),
             cuicui_layout_bevy_ui::Plugin,
             // bevy_inspector_egui::quick::WorldInspectorPlugin::default(),
